00e45218fdc8a9a4dab1ec2eaa072f23af3658e4
MD Shakib <126575973+mdshakibsami@users.noreply.github.com>
2024-12-11 05:18:36 -0500

    Add files via upload
    First upload

---
+++ Chef_and_Socks.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+const int MOD = 1e9+7;
+signed main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    
+    int a,x,y; cin>>a>>x>>y;
+    if(a<=x+y) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
